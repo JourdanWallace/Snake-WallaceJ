6d212248ee633aafea9e1f2cff622f719b4204d8
Jourdan Wallace <jaywallace713@gmail.com>
2014-10-14 08:42:59 -0700

    Got my snake moving, now I'm going to begin creating the food!

---
+++ js/snake.js
@@ -0,0 +1,65 @@
+var snake;
+var snakeLength;
+var snakeSize;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+snakeInitialize();
+setInterval(gameLoop, 1000/30);
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(0,245,90)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var index = snakeLength - 1; index >= 0; index--) {
+        snake.push( {
+            x: index,
+            y: 0
+        });            
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "blue";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }    
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop(); 
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
