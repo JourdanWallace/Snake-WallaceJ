d5bfb05d11a17ea8a14344f11e7ef20f119db9cf
Jourdan Wallace <jaywallace713@gmail.com>
2014-10-16 09:19:51 -0700

    Almost down with the videos!

--- js/snake.js
+++ js/snake.js
@@ -6,6 +6,7 @@
 var snake;
 var snakeLength;
 var snakeSize;
+var snakeDirection;
 
 var food;
 
@@ -60,6 +61,7 @@ function snakeInitialize() {
     snake = [];
     snakeLength = 5;
     snakeSize = 20;
+    snakeDirection = "down";
     
     for(var index = snakeLength - 1; index >= 0; index--) {
         snake.push( {
@@ -80,7 +82,9 @@ function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    snakeHeadX++;
+    if(snakeDirection == "down") {
+        snakeHeadY++;
+    }
     
     var snakeTail = snake.pop(); 
     snakeTail.x = snakeHeadX;
