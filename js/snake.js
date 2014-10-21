a6384451bedc133e40cf5a1c953558b2b64ba5ac
Jourdan Wallace <jaywallace713@gmail.com>
2014-10-21 09:20:30 -0700

    Today was a great day, i finally got all my arrow keys working so now I'm working on eating my food. Making progress!!!!

--- js/snake.js
+++ js/snake.js
@@ -84,12 +84,19 @@ function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    if(snakeDirection == "down") {
+    {   
+    if(snakeDirection == "down") 
         snakeHeadY++;
-    }
-    else if(snakeDirection == "right") {
+    
+    else if(snakeDirection == "right") 
         snakeHeadX++; 
-    }
+    
+    if(snakeDirection == "up") 
+        snakeHeadY--;
+    
+    else if(snakeDirection == "left") 
+        snakeHeadX--;
+    } 
     
     var snakeTail = snake.pop(); 
     snakeTail.x = snakeHeadX;
@@ -112,15 +119,15 @@ function foodInitialize() {
 
 function foodDraw() {
     context.fillStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
 
 function setFoodPosition() {
    var randomX = Math.floor(Math.random() * screenWidth);
    var randomY = Math.floor(Math.random() * screenHeight);
    
-   food.x = randomX;
-   food.y = randomY;
+   food.x = Math.floor(randomX / snakeSize);
+   food.y = Math.floor(randomY / snakeSize);
 }
 
 /* --------------------------------------------------------------------------
@@ -128,10 +135,29 @@ function setFoodPosition() {
  * --------------------------------------------------------------------------
  */
 function keyboardHandler(event) {
-    console.log(event);
+    console.log(event); 
     
-    if (event.keyCode == "39") {
+    {if (event.keyCode == "37" && snakeDirection != "right") 
+         snakeDirection = "left";
+    
+    else if(event.keyCode == "38" && snakeDirection != "down") 
+         snakeDirection = "up";
+    
+    if (event.keyCode == "39" && snakeDirection != "left") 
          snakeDirection = "right";
+       
+    else if(event.keyCode == "40" && snakeDirection != "up") 
+         snakeDirection = "down";
+     }
+     }
+     
+/* --------------------------------------------------------------------------
+ * Collision Handling
+ * --------------------------------------------------------------------------
+ */
+
+function checkFoodCollisions() {
+    if() {
+        
     }
-    else if(event.keyCode == "40")
 }
