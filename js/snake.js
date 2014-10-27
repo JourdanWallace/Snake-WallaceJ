c11e7caea65cb16931dfdcd90be74250de536957
Jourdan Wallace <jaywallace713@gmail.com>
2014-10-27 10:45:29 -0700

    I'm on pace with the videos, now just trying to get the game over screen to work.

--- js/snake.js
+++ js/snake.js
@@ -15,6 +15,7 @@ var screenWidth;
 var screenHeight;
 
 var gameState;
+var gameOverMenu;
 
 /* ---------------------------------------------------------------------------
  * Executing Game Code
@@ -42,6 +43,8 @@ function gameInitialize() {
     canvas.height = screenHeight;
 
     document.addEventListener("keydown", keyboardHandler);
+    
+    gameOverMenu = document.getElementById("gameOver");
     
     setState("PLAY");
 }
@@ -197,4 +200,15 @@ function checkSnakeCollisions(snakeHeadX, snakeHeadY) {
 
 function setState(state) {
     gameState = state;
+    showMenu(state);
+}
+
+function displayMenu(menu) {
+    menu.style.visibility = "visible";
+}
+
+function showMenu(state) {
+    if(state == "GAME OVER") {
+     displaymenu(gameOverMenu);   
+    }
 }
